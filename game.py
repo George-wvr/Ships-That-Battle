b5383ce3c4713e3a102e79b8b05fac7e08637d4e
George <162292571+George-wvr@users.noreply.github.com>
2024-03-07 13:47:52 +0000

    Creating game box
    Can only move the boat in the game box

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ game_box_col = (175, 171, 171)
 #Variables and constants
 swidth = 1250
 sheight = 600
-boat_speed = 1.5
+boat_speed = 0.117
 enemy_boat_speed = 0.5
 cool_down = 0  # the cooldown for between missile fires
 score = 0
@@ -62,8 +62,8 @@ class Boat(pygame.sprite.Sprite):
         super().__init__()
         self.x = x_pos
         self.y = y_pos
-        self.width = 25
-        self.height = 25
+        self.width = 10
+        self.height = 10
         self.boat = pygame.Surface((self.width, self.height),8)
         self.rect = self.boat.get_rect(center=(self.x, self.y))
 
@@ -83,28 +83,28 @@ class Boat(pygame.sprite.Sprite):
     def move(self):
         pressedkey = pygame.key.get_pressed()
 
-        if pressedkey[K_a]:
-            if self.rect.left > 0:
+        if pressedkey[K_a] or pressedkey[K_LEFT]:
+            if self.rect.left > 275:
                 # print("left")
                 self.x -= boat_speed
 
-        if self.rect.left < swidth - (self.width):
-            if pressedkey[K_d]:
+        if self.rect.left < 1225 - (self.width):
+            if pressedkey[K_d] or pressedkey[K_RIGHT]:
                 # print("right")
                 self.x += boat_speed
 
-        if self.rect.top > 0:
-            if pressedkey[K_w]:
+        if self.rect.top > 25:
+            if pressedkey[K_w] or pressedkey[K_UP]:
                 # print("up")
                 self.y -= boat_speed
 
-        if self.rect.top < sheight - self.height:
-            if pressedkey[K_s]:
+        if self.rect.top < 575 - self.height:
+            if pressedkey[K_s] or pressedkey[K_DOWN]:
                 # print("down")
                 self.y += boat_speed
 
 #Players Boat
-player_boat = Boat(10,10)
+player_boat = Boat(280, 30)
 
 #Screen design
 def layout(displaysurf):
@@ -122,11 +122,30 @@ def layout(displaysurf):
     #Score
     displaysurf.blit(box, (25,225))
 
+    #High Score
+    displaysurf.blit(box, (25,525))
+
     #Box Borders
-    box = pygame.Surface((225,50))
+    #Timer
     box_rect = box.get_rect(topleft = (25,25))
     pygame.draw.rect(displaysurf, black, box_rect, 5)
 
+    #Health
+    box_rect = box.get_rect(topleft = (25,125))
+    pygame.draw.rect(displaysurf, black, box_rect, 5)
+
+    #Score
+    box_rect = box.get_rect(topleft = (25,225))
+    pygame.draw.rect(displaysurf, black, box_rect, 5)
+
+    #High Score
+    box_rect = box.get_rect(topleft = (25,525))
+    pygame.draw.rect(displaysurf, black, box_rect, 5)
+
+    #Game Space
+    box = pygame.Surface((950,550))
+    box.fill(sea_col)
+    displaysurf.blit(box,(275,25))
 
 #Main Function for the game
 def run_game(displaysurf):
